ff47cb3b1b02d8ae1714c63f324b9c96b826a742
dongjiang1989 <dongjiang1989@126.com>
2022-12-29 22:07:06 +0800

    dongjiang, update path

--- hack/build/start.sh
+++ hack/build/start.sh
@@ -5,21 +5,14 @@ nsenter -m/proc/1/ns/mnt fusermount -u /var/lib/lxcfs 2> /dev/null || true
 nsenter -m/proc/1/ns/mnt [ -L /etc/mtab ] || \
         sed -i "/^lxcfs \/var\/lib\/lxcfs fuse.lxcfs/d" /etc/mtab
 
-# remove /var/lib/lxcfs
-rm -rf /var/lib/lxcfs/*
-
 # Prepare
 mkdir -p /usr/local/lib/lxcfs /var/lib/lxcfs
 
 # Update lxcfs
-cp -f /lxcfs/lxcfs /usr/local/bin/lxcfs
-cp -f /lxcfs/liblxcfs.so /usr/local/lib/lxcfs/liblxcfs.so
+cp -f /lxcfs/src/lxcfs /usr/local/bin/lxcfs
+cp -f /lxcfs/src/.libs/liblxcfs.so /usr/local/lib/lxcfs/liblxcfs.so
+cp -f /lxcfs/src/liblxcfs.la /usr/local/lib/lxcfs/liblxcfs.la
 
-cp -f /lxcfs/libfuse.so.2.9.2 /usr/lib64/libfuse.so.2.9.2
-cp -f /lxcfs/libulockmgr.so.1.0.1 /usr/lib64/libulockmgr.so.1.0.1
-
-ln -s /usr/lib64/libfuse.so.2.9.2 /usr/lib64/libfuse.so.2
-ln -s /usr/lib64/libulockmgr.so.1.0.1 /usr/lib64/libulockmgr.so.1
 
 # Mount
 exec nsenter -m/proc/1/ns/mnt /usr/local/bin/lxcfs /var/lib/lxcfs/ --enable-cfs -l
