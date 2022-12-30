3827f02ea0efe870536edcc9b877468a32abe16a
dongjiang1989 <dongjiang1989@126.com>
2022-12-30 12:29:50 +0800

    dongjiang, fix bug

--- pkg/webhook/webhook.go
+++ pkg/webhook/webhook.go
@@ -188,14 +188,13 @@ func (whsvr *WebhookServer) Serve(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	glog.Infof("Serve Respone AdmissionReview: %v\n", admissionReview)
-
 	resp, err := json.Marshal(admissionReview)
 	if err != nil {
 		glog.Errorf("Can't encode response: %v", err)
 		http.Error(w, fmt.Sprintf("could not encode response: %v", err), http.StatusInternalServerError)
 		return
 	}
+	glog.Infof("Serve Respone AdmissionReview: %v\n", string(resp))
 	glog.Infof("Ready to write response ...")
 	if _, err := w.Write(resp); err != nil {
 		glog.Errorf("Can't write response: %v", err)
