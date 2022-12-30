e594d569be38ae4fed8c58e635428606e38619fa
dongjiang1989 <dongjiang1989@126.com>
2022-12-30 11:38:27 +0800

    dongjiang, add case

--- pkg/webhook/webhook.go
+++ pkg/webhook/webhook.go
@@ -161,7 +161,7 @@ func (whsvr *WebhookServer) Serve(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	glog.Infof("Received AdmissionReview: %s\n", string(body))
+	glog.Infof("Serve Received AdmissionReview: %s\n", string(body))
 
 	var admissionResponse *v1beta1.AdmissionResponse
 	ar := v1beta1.AdmissionReview{}
@@ -184,21 +184,24 @@ func (whsvr *WebhookServer) Serve(w http.ResponseWriter, r *http.Request) {
 	admissionReview := v1beta1.AdmissionReview{}
 	if admissionResponse != nil {
 		admissionReview.Response = admissionResponse
-		if ar.Request != nil {
-			admissionReview.Response.UID = ar.Request.UID
-		}
 	}
+
+	glog.Infof("Serve Respone AdmissionReview: %v\n", admissionReview)
 
 	resp, err := json.Marshal(admissionReview)
 	if err != nil {
 		glog.Errorf("Can't encode response: %v", err)
 		http.Error(w, fmt.Sprintf("could not encode response: %v", err), http.StatusInternalServerError)
+		return
 	}
 	glog.Infof("Ready to write response ...")
 	if _, err := w.Write(resp); err != nil {
 		glog.Errorf("Can't write response: %v", err)
 		http.Error(w, fmt.Sprintf("could not write response: %v", err), http.StatusInternalServerError)
+		return
 	}
+	w.WriteHeader(http.StatusOK)
+	return
 }
 
 // main mutation process
